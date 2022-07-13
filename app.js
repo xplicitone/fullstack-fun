f4f36c4210de401f2ad20bc2aad9ce549ecba3e9
xplicitone <paul.kalaw@gmail.com>
2022-07-12 22:14:28 -0500

    add demo path with custom header

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ app.get('/', (req, res) => {
   res.send('Hello World!')
 })
 
+app.get('/demo', (req, res) => {
+  res.set('X-custom-header-here', 'what is up dog');
+  res.status(418);
+  res.send('I prefer coffee');
+})
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
 })
